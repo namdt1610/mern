ab67fb67752bb84f9872d144a111693f537ab560
namdt1610 <88128203+namdt1610@users.noreply.github.com>
2024-05-26 06:48:00 +0000

    chore: Remove unused CSS code

--- client/src/client/views/login/Login.jsx
+++ client/src/client/views/login/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header'
+import PasswordInput from '../../components/Inputs/PasswordInput'
 
 const Login = () => {
     return (
         <>
             <Header />
-            <div className="flex items-center justify-center mt-28">
-                <div>
+            <div className="flex items-center justify-center mt-28 ">
+                <div className='w-96 border rounded-3xl bg-white px-7 py-10'>
                     <form onSubmit={() => {}}>
                         <h4 className="text-2xl mb-7">Login</h4>
                         <input
@@ -15,12 +16,15 @@ const Login = () => {
                             placeholder="Email"
                             className="input-box"
                         />
+
+                        <PasswordInput />
+
                         <button type="submit" className="btn-primary">
                             Login
                         </button>
                         <p className="text-sm text-center mt-4">
                             Not a member?{' '}
-                            <Link to="/register" className="text-blue-500">
+                            <Link to="/register" className="underline text-primary">
                                 Register
                             </Link>
                         </p>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#FF6363',
+                secondary: '#EF863E'
+            },
+        },
     },
     plugins: [],
 }
