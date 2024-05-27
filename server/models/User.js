647371c765344778e5cac989683a56d093f0ef36
namdt1610 <88128203+namdt1610@users.noreply.github.com>
2024-05-27 07:10:03 +0000

    nothing

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
-}, {
-  timestamps: true,
-});
+const userSchema = new schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: false,
+            default: null,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
+        loginAt: {
+            type: Date,
+            default: Date.now,
+        },
+        logoutAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+const User = mongoose.model('users', userSchema)
+module.exports = User
